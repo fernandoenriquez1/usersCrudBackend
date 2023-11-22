93cfdb98487d6f00a1724451a8d5a9b89ecbcf10
fernandoenriquez1 <109874935+fernandoenriquez1@users.noreply.github.com>
2023-11-22 08:33:18 -0700

    add service registration
    added service registration from iRepository to BaseRepository

---
+++ Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,26 @@
+﻿using Application.Contracts;
+using Infrastructure.Persistence;
+using Infrastructure.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<UserCrudDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("ConnectionString")));
+
+            services.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
+
+            return services;
+        }
+    }
+}
